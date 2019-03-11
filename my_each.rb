582fa92ff21c784deb6c03166de94bbe05f7fda9
Jeff M <jkdmarks@gmail.com>
2019-03-11 19:23:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,19 @@
-def my_each(array)
+def my_collect(array)
+  output_array = []
   i = 0
   while i < array.length
-    yield array[i]
+    yield output_array.push(array[i])[i]
     i += 1
   end
-  array
+  output_array
 end
 
-collection = [1, 2, 3, 4]
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
-my_each(collection) do |i|
-  i
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
 end
